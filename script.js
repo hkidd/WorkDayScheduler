e80e072628a3201569087f6b799d81e1bf0f306b
hkidd <harrisonakidd@gmail.com>
2021-07-01 17:33:28 -0500

    Added event listeners for all save buttons.

--- script.js
+++ script.js
@@ -57,4 +57,98 @@ bckgrdColor(timeArray);
     // Clicking the save button saves that block to local storage
     // setItem (saving) and getItem (retrieval upon page reload)
     // Grab eventInput value and save to local storage
+    // A different storage item for each timeblock?
+
+    var saveBtn1 = document.querySelector('#save1');
+    var saveBtn2 = document.querySelector('#save2');
+    var saveBtn3 = document.querySelector('#save3');
+    var saveBtn4 = document.querySelector('#save4');
+    var saveBtn5 = document.querySelector('#save5');
+    var saveBtn6 = document.querySelector('#save6');
+    var saveBtn7 = document.querySelector('#save7');
+    var saveBtn8 = document.querySelector('#save8');
+    var saveBtn9 = document.querySelector('#save9');
+
+    saveBtn1.addEventListener("click", saveTimeblock1);
+    saveBtn2.addEventListener("click", saveTimeblock2);
+    saveBtn3.addEventListener("click", saveTimeblock3);
+    saveBtn4.addEventListener("click", saveTimeblock4);
+    saveBtn5.addEventListener("click", saveTimeblock5);
+    saveBtn6.addEventListener("click", saveTimeblock6);
+    saveBtn7.addEventListener("click", saveTimeblock7);
+    saveBtn8.addEventListener("click", saveTimeblock8);
+    saveBtn9.addEventListener("click", saveTimeblock9);
+
+    function saveTimeblock1(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock1");
+    }
+
+    function saveTimeblock2(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock2");
+    }
+
+    function saveTimeblock3(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock3");
+    }
+
+    function saveTimeblock4(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock4");
+    }
+
+    function saveTimeblock5(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock5");
+    }
+
+    function saveTimeblock6(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock6");
+    }
+
+    function saveTimeblock7(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock7");
+    }
+
+    function saveTimeblock8(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock8");
+    }
+
+    function saveTimeblock9(event) {
+        event.stopPropagation();
+        var val = $(this).closest("div.input-group").find("input[name='eventValue']").val();
+        console.log(val);
+
+        localStorage.setItem("eventBlock9");
+    }
 
